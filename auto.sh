bb943e96ac7ff5059010c40b265169f2fdb12bec
Scott Walker <walker8@localhost.localdomain>
2017-07-27 03:02:25 -0400

    updated auto.sh based on tuning results

--- auto.sh
+++ auto.sh
@@ -1,25 +1,25 @@
-EXP="exp1"
-NITER=20000
+EXP="exp2"
+NITER=200000
 TURBO="t"
 PSTATE="0x2d00"
-DUTY=10000
+DUTY=2400
 PART=4
 MAXFREQ="4.2" # should be set to max non-turbo frequency
 FSPATH="~/Firestarter-Experimental/FIRESTARTER" # should point to firestarter executable
-TDP=205
+TDP=205 # set to tdp
 CPS=8 #cores per socket
 
-START=$TDP * 0.6
-END=$TDP #set to tdp
-LPOW=60
-LSEC=8
+START=1
+END=32
+LPOW=187
+LSEC=1
 SEC=25
 POW=$TDP # set to tdp
 CTR=1
 
 #for ((POW=60; POW <= 105; POW += 5));
 #do
-	for ((LPOW=START; LPOW<=END; LPOW += 5));
+	for ((LSEC=START; LSEC<=END; LSEC *= 2, DUTY *= 2));
 	do
 		echo -e "$NITER\n$LSEC\n$LPOW.0\n$SEC\n$POW.0\n$PSTATE\n$TURBO\n$DUTY\n$PART\n$MAXFREQ\n$CPS" > fsconfig
 		DPATH="data/$EXP/$LPOW/$LSEC/$POW/$SEC/$PART/$DUTY"
